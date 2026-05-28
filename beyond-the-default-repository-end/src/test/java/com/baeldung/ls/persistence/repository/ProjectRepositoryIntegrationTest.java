abd6706ec844942172cceeef92e6e33208d0468e
Baeldung CI User <baeldungci@baeldung.com>
2026-05-28 23:14:21 +0000

    use EmbeddedDatabaseBuilder in M5 L4 end project PersistenceConfig

--- beyond-the-default-repository-end/src/test/java/com/baeldung/ls/persistence/repository/ProjectRepositoryIntegrationTest.java
+++ beyond-the-default-repository-end/src/test/java/com/baeldung/ls/persistence/repository/ProjectRepositoryIntegrationTest.java
@@ -50,7 +50,7 @@ public class ProjectRepositoryIntegrationTest {
     @Test
     public void givenProjectCreated_whenFindByDateCreatedBetween_thenSuccess() {
         Project oldProject = new Project(randomAlphabetic(6), LocalDate.now()
-            .minusYears(1));
+                .minusYears(1));
         projectRepository.save(oldProject);
 
         Project newProject = new Project(randomAlphabetic(6), LocalDate.now());
@@ -60,9 +60,9 @@ public class ProjectRepositoryIntegrationTest {
         projectRepository.save(newProject2);
 
         List<Project> retreivedProjects = projectRepository.findByDateCreatedBetween(LocalDate.now()
-            .minusDays(1),
-            LocalDate.now()
-                .plusDays(1));
+                .minusDays(1),
+                LocalDate.now()
+                        .plusDays(1));
 
         assertThat(retreivedProjects).contains(newProject, newProject2);
     }
--- spring-with-jpa-and-hibernate-end/src/main/java/com/baeldung/ls/config/PersistenceConfig.java
+++ spring-with-jpa-and-hibernate-end/src/main/java/com/baeldung/ls/config/PersistenceConfig.java
@@ -4,12 +4,22 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @Configuration
 public class PersistenceConfig {
 
     @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .setName("learn-spring-db")
+            .build();
+    }
+
+    // DriverManagerDataSource alternative
+    /*@Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
@@ -17,7 +27,7 @@ public class PersistenceConfig {
         dataSource.setUsername("username");
         dataSource.setPassword("password");
         return dataSource;
-    }
+    }*/
 
     // MySQL DataSource bean
     /*@Bean
